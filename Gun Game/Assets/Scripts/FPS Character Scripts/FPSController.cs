c45e420a6d127c41db8dd70883ffda9b86e880c9
sunny.chao <Sunny.Chao@deutschinc.com>
2017-09-22 16:18:40 -0400

    added jump

--- Gun Game/Assets/Scripts/FPS Character Scripts/FPSController.cs
+++ Gun Game/Assets/Scripts/FPS Character Scripts/FPSController.cs
@@ -11,6 +11,7 @@ public class FPSController : MonoBehaviour {
 	public float runSpeed = 10f;
 	public float crouchSpeed = 4f;
 	public float jumpSpeed = 8f;
+	public float jumpMoveSpeed = 4f;
 	public float gravity = 20f;
 
 	private float speed;
@@ -25,6 +26,13 @@ public class FPSController : MonoBehaviour {
 	private CharacterController charController;
 	private Vector3 moveDirection = Vector3.zero;
 
+	public LayerMask groundLayer;
+	private float rayDistance;
+	private float default_ControllerHeight;
+	private Vector3 default_CamPos;
+	private float camHeight;
+
+
 	// Use this for initialization
 	void Start () {
 		firstPerson_View = transform.Find ("FPS View").transform; // transform.find goes through gameobject children to find name
@@ -32,6 +40,9 @@ public class FPSController : MonoBehaviour {
 		speed = walkSpeed;
 		is_Moving = false;
 
+		rayDistance = charController.height * .5f + charController.radius; //detects if we are on the ground when crouching
+		default_ControllerHeight = charController.height;
+		default_CamPos = firstPerson_View.localPosition;
 	}
 	
 	// Update is called once per frame
@@ -69,12 +80,86 @@ public class FPSController : MonoBehaviour {
 		firstPerson_View.localEulerAngles = firstPerson_View_Rotation; //fps cam rotation angles are relative to parent object fps player. we need to use local euler angles to affect angles relative to parent object and not relative to the world
 
 		if(is_Grounded){
+			PlayerCrouchingAndSprinting ();
 			moveDirection = new Vector3 (inputX * inputModifyFactor, -antiBumpFactor, inputY * inputModifyFactor); //anitbumpfactor will smooth movement when bumping into wall
 			moveDirection = transform.TransformDirection (moveDirection) * speed; // converting direction from local space to world space
+			PlayerJump ();
 		}
 		moveDirection.y -= gravity * Time.deltaTime;
 		is_Grounded = (charController.Move (moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0; //Move returns a collision flag. collision flags is a bit number. if collision flag is not 0, it means we are ground and returns true
 
 		is_Moving = charController.velocity.magnitude > .15f;
+	}
+
+	void PlayerCrouchingAndSprinting(){
+		if(Input.GetKeyDown(KeyCode.LeftControl)){
+			if(!is_Crouching){
+				is_Crouching = true;
+			}else{
+				if(CanGetUp()){
+					is_Crouching = false;
+				}
+			}
+			StopCoroutine (MoveCameraCrouch ());
+			StartCoroutine (MoveCameraCrouch ());
+		}
+
+		if(Input.GetKeyUp(KeyCode.LeftControl)){
+			if(is_Crouching){
+				is_Crouching = false;
+			}
+			StopCoroutine (MoveCameraCrouch ());
+			StartCoroutine (MoveCameraCrouch ());
+		}
+
+		if(is_Crouching){
+			speed = crouchSpeed;
+		}else{
+			if(Input.GetKey(KeyCode.LeftShift)){
+				speed = crouchSpeed;
+			}else{
+				speed = walkSpeed;
+			}
+		}
+	}
+
+	bool CanGetUp(){
+		Ray groundRay = new Ray (transform.position, transform.up);
+		RaycastHit groundHit;
+
+		if(Physics.SphereCast(groundRay, charController.radius + .05f, out groundHit, rayDistance, groundLayer)){ //a sphere that is half the size of player. detects position of sphere to determine if crouching or not. if groundray hits ground layer
+			if(Vector3.Distance(transform.position, groundHit.point) < 2.3f){ //if current position and the point of contact of ray case is less than 2.3f
+				return false;
+			}
+		}
+		return true;
+	}
+
+	IEnumerator MoveCameraCrouch(){
+		charController.height = is_Crouching ? default_ControllerHeight / 1.5f : default_ControllerHeight;
+		charController.center = new Vector3 (0f, charController.height / 2f, 0f);
+
+		camHeight = is_Crouching ? default_CamPos.y / 1.5f : default_CamPos.y; //moves camera down when crouching or return to original height
+
+		while(Mathf.Abs(camHeight - firstPerson_View.localPosition.y) > .01f){
+			firstPerson_View.localPosition = Vector3.Lerp (firstPerson_View.localPosition, new Vector3 (default_CamPos.x, camHeight, default_CamPos.z), Time.deltaTime * 11f);
+			yield return null;
+		}
+	}
+
+	void PlayerJump(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			if(is_Crouching){
+				if(CanGetUp()){
+					is_Crouching = false;
+					StopCoroutine (MoveCameraCrouch ());
+					StartCoroutine (MoveCameraCrouch ());
+				}
+			}else{
+				moveDirection.y = jumpSpeed;
+			}
+
+		}
+
 	}
 }	
