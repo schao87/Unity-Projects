5c1cc29bdfeb830033b04abd78a1778cfeeb7c78
sunny.chao <Sunny.Chao@deutschinc.com>
2017-09-22 12:45:11 -0400

    adding gun game

---
+++ Gun Game/Assets/Scripts/FPS Character Scripts/FPSController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour {
+	private Transform firstPerson_View;
+	private Transform firstPerson_Camera;
+	private Vector3 firstPerson_View_Rotation = Vector3.zero;
+
+	public float walkSpeed = 6.75f;
+	public float runSpeed = 10f;
+	public float crouchSpeed = 4f;
+	public float jumpSpeed = 8f;
+	public float gravity = 20f;
+
+	private float speed;
+	private bool is_Moving, is_Grounded, is_Crouching;
+
+	private float inputX, inputY;
+	private float inputX_Set, inputY_Set;
+	private float inputModifyFactor;
+
+	private bool limitDiagonalSpeed = true;
+	private float antiBumpFactor = .75f;
+	private CharacterController charController;
+	private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		firstPerson_View = transform.Find ("FPS View").transform; // transform.find goes through gameobject children to find name
+		charController = GetComponent<CharacterController> ();
+		speed = walkSpeed;
+		is_Moving = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerMovement ();
+	}
+
+	void PlayerMovement(){
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.S)) {
+			if(Input.GetKey(KeyCode.W)){
+				inputY_Set = 1f;
+			}else{
+				inputY_Set = -1f;
+			}
+		}else{
+			inputY_Set = 0f;
+		}
+
+		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+			if(Input.GetKey(KeyCode.A)){
+				inputX_Set = -1f;
+			}else{
+				inputX_Set = 1f;
+			}
+		}else{
+			inputX_Set = 0f;
+		}
+
+		inputY = Mathf.Lerp (inputY, inputY_Set, Time.deltaTime * 19f);
+		inputX = Mathf.Lerp (inputX, inputX_Set, Time.deltaTime * 19f);
+
+		inputModifyFactor = Mathf.Lerp(inputModifyFactor, (inputY_Set != 0 && inputX_Set != 0 && limitDiagonalSpeed) ? .75f : 1.0f, Time.deltaTime * 19f); //limits the speed of diagonal movement
+
+		firstPerson_View_Rotation = Vector3.Lerp (firstPerson_View_Rotation, Vector3.zero, Time.deltaTime * 5f);
+		firstPerson_View.localEulerAngles = firstPerson_View_Rotation; //fps cam rotation angles are relative to parent object fps player. we need to use local euler angles to affect angles relative to parent object and not relative to the world
+
+		if(is_Grounded){
+			moveDirection = new Vector3 (inputX * inputModifyFactor, -antiBumpFactor, inputY * inputModifyFactor); //anitbumpfactor will smooth movement when bumping into wall
+			moveDirection = transform.TransformDirection (moveDirection) * speed; // converting direction from local space to world space
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		is_Grounded = (charController.Move (moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0; //Move returns a collision flag. collision flags is a bit number. if collision flag is not 0, it means we are ground and returns true
+
+		is_Moving = charController.velocity.magnitude > .15f;
+	}
+}	
---
+++ Gun Game/Assets/Scripts/FPS Character Scripts/FPSMouse.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMouse : MonoBehaviour {
+
+	public enum RotationAxes { MouseX, MouseY}
+	public RotationAxes axes = RotationAxes.MouseY; //by default it will rotate around mouse Y
+
+	private float currentSensitivity_X = 1.5f;
+	private float currentSensitivity_Y = 1.5f;
+
+	private float sensitivity_X = 1.5f;
+	private float sensitivity_Y = 1.5f;
+
+	private float rotation_X, rotation_Y;
+
+	private float minimum_X = -360f;
+	private float maximum_X = 360f;
+
+	private float minimum_Y = -60f;
+	private float maximum_Y = 60f;
+
+	private Quaternion originalRotation;
+
+	private float mouseSensitivity = 1.7f;
+
+	// Use this for initialization
+	void Start () {
+		originalRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate(){ //late update is called after updated and fixed update
+		HandleRotation();
+	}
+	float ClampAngle(float angle, float min, float max){
+		if(angle < -360f){
+			angle -= 360f;
+		}
+
+		if(angle > 360f){
+			angle -= 360f;
+		}
+		return Mathf.Clamp(angle, min, max);
+	}
+	
+	void HandleRotation(){
+		if(currentSensitivity_X != mouseSensitivity || currentSensitivity_Y != mouseSensitivity){
+			currentSensitivity_X = currentSensitivity_Y = mouseSensitivity; //this is a precaution to test sensitivity
+		}
+
+		sensitivity_X = currentSensitivity_X;
+		sensitivity_Y = currentSensitivity_Y;
+
+		if(axes == RotationAxes.MouseX){
+			rotation_X += Input.GetAxis ("Mouse X") * sensitivity_X; //mouse x is horizontal movement of mouse
+
+			rotation_X = ClampAngle (rotation_X, minimum_X, maximum_X);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotation_X, Vector3.up); // uses mouse x degrees and rotates it around the world y axis
+			transform.localRotation = originalRotation * xQuaternion;
+		}
+
+		if(axes == RotationAxes.MouseY){
+			rotation_Y += Input.GetAxis ("Mouse Y") * sensitivity_Y; //mouse y is verticle movement of mouse
+
+			rotation_Y = ClampAngle (rotation_Y, minimum_Y, maximum_Y);
+			Quaternion yQuaternion = Quaternion.AngleAxis (-rotation_Y, Vector3.right);// uses mouse y degrees and rotates it around the world x axis
+			transform.localRotation = originalRotation * yQuaternion;
+		}
+	}
+}
